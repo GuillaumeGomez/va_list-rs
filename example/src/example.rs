14b2273183cceda0acf4f427dde7d8f0303d1e7a
Guillaume Gomez <guillaume1.gomez@gmail.com>
2016-05-18 08:22:46 +0200

    Working version

--- example/src/example.rs
+++ example/src/example.rs
@@ -7,9 +7,8 @@ extern "C" {
     fn vprintf(f: *const c_char, ...) -> c_int;
 }
 
-#[test]
-fn test1() {
+fn main() {
     to_va_list!(|v: va_list::va_list| {
-        vprintf(b"%d %d\n\0".as_ptr() as *const c_char, v);
-    }, 1, 2);
+        vprintf(b"%d %d %s\n\0".as_ptr() as *const c_char, v);
+    }, 1, 2, b"salut!\0".as_ptr());
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,63 @@
 #![feature(type_ascription)]
 #![allow(dead_code)]
+#![allow(private_in_public)]
 
 extern crate libc;
 
-use libc::{c_uint, c_void};
-
-use std::mem::transmute;
+#[macro_export]
+macro_rules! counter {
+    ($arg:expr) => {{
+        1
+    }};
+    ($arg:expr, $arg2:expr) => {{
+        2
+    }};
+    ($arg:expr, $arg2:expr, $($args:expr),*) => {{
+        counter!($($args),*) + 2
+    }};
+    ($arg:expr, $($args:expr),*) => {{
+        counter!($($args),*) + 1
+    }}
+}
 
 #[macro_export]
 macro_rules! to_va_list {
     ($func:expr, $($args:expr),*) => {{
-        let v = vec!($($args),*);
+        unsafe extern "C" fn call_func(f: *mut libc::c_void, ap: $crate::va_list) {
+            let f: &Box<Fn($crate::va_list) + 'static> = std::mem::transmute(f);
+            f(ap);
+        }
+
+        #[repr(C)]
+        pub struct Wrap {
+            pub f: extern "C" fn(*mut libc::c_void, $crate::va_list),
+            pub c: *mut libc::c_void,
+            pub len: c_uint,
+        }
+
+        extern "C" {
+            fn create_va_list(w: *mut Wrap, ...);
+        }
+
         unsafe {
-            $crate::create_va_list($crate::call_func, $crate::convert_closure($func),
-                                   v.len() as c_uint, $($args),*);
+            let fu = $func;
+            let wrap = Wrap {
+                f: std::mem::transmute(call_func as usize),
+                c: $crate::convert_closure(fu),
+                len: counter!($($args),*),
+            };
+            create_va_list(Box::into_raw(Box::new(wrap)), $($args),*);
         }
     }}
 }
 
 #[doc(hidden)]
-pub fn convert_closure<F: Fn(*mut va_list)>(f: F) -> *mut c_void {
-    let f: Box<Box<F>> = Box::new(Box::new(f));
+pub fn convert_closure<F: Fn(va_list) + 'static>(f: F) -> *mut libc::c_void {
+    let f: Box<Box<Fn(va_list) + 'static>> = Box::new(Box::new(f));
     Box::into_raw(f) as *mut _
 }
 
-#[doc(hidden)]
-pub fn call_func(f: *mut c_void, ap: va_list) {
-    let f: &Box<Fn(va_list) + 'static> = unsafe { transmute(f) };
-    f(ap);
-}
-
 #[repr(C)]
-pub struct va_list;
-
-extern "C" {
-    pub fn create_va_list(f: fn(*mut c_void, va_list), a: *mut c_void, n: c_uint, ...);
-}
+struct _va_list;
+#[allow(non_camel_case_types)]
+pub type va_list = *mut _va_list;
