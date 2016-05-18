a7abf5faa50b85f4b5a83bc8d4b56cb467c86ae8
Guillaume Gomez <guillaume1.gomez@gmail.com>
2016-05-18 05:35:44 +0200

    Go full macro, missing the C part

--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,26 @@
 extern crate libc;
-extern crate c_vec;
 
 use libc::c_void;
 
+#[macro_export]
 macro_rules! caller {
-    ($func:expr, $size:expr, $list:expr) => (
-        if let Some(el) = $list.pop() {
-            caller!($func, $list, $size, el)
-        } else {
-            call_func($func, $size)
-        }
-    );
-    ($func:expr, $size:expr, $list:expr, $arg:expr) => (
-        if let Some(el) = $list.pop() {
-            caller!($func, $list, $size, el, $arg)
-        } else {
-            call_func($func, $size)
-        }
-    );
-    ($func:expr, $size:expr, $list:expr, $($args:expr),*) => (
-        if let Some(el) = $list.pop() {
-            caller!($func, $list, el, $($args),*)
-        } else {
-            call_func($func, $size, $($args),*)
-        }
-    )
+    ($func:expr, $nb_args:expr, $($args:expr),*) => {{
+        c_func(convert_closure($func), $nb_args, $($args:expr),*);
+    }}
 }
 
-pub struct VaList {
-    args: Vec<*mut c_void>,
+fn convert_closure<F: Fn(*mut va_list)>(f: F) -> *mut c_void {
+    let f: Box<Box<F>> = Box::new(Box::new(f));
+    Box::into_raw(f) as *mut _
 }
 
-impl VaList {
-    pub fn new() -> VaList {
-        VaList {
-            args: vec!(),
-        }
-    }
-
-    pub fn add_arg<T>(&mut self, arg: &T) {
-        self.push(arg as *mut _ as *mut c_void);
-    }
-
-    pub fn to_c_va_list<F: Fn(*mut va_list)>(self, func: F) {
-        let f = transmute(func);
-        caller!(func, self.args.len(), self.args);
-    }
-}
+pub struct va_list;
 
 // part to call in C directly
-extern "C" fn call_func(f: *mut c_void, num_args: u32, ...) {
+/*extern "C" fn call_func(f: *mut c_void, num_args: u32, ...) {
     let f: &Box_<Fn(*mut va_list) + 'static> = transmute(f);
     va_list ap;
     va_start(ap, num_args);
     f(ap);
     va_end(ap);
-}
+}*/
