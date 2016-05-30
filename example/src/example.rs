1e8e4046344c6d54bd10f557d2ebfd4523f735e5
ggomez <guillaume1.gomez@gmail.com>
2016-05-30 18:58:28 +0200

    Force user to use unsafe block

--- example/src/example.rs
+++ example/src/example.rs
@@ -8,9 +8,11 @@ extern "C" {
 }
 
 fn main() {
-    to_va_list!(|v: va_list::va_list| {
-        vprintf(b"%d %d %s\n\0".as_ptr() as *const c_char, v);
-    }, 1 as c_int, 2 as c_int, b"salut!\0".as_ptr());
+    unsafe {
+        to_va_list!(|v: va_list::va_list| {
+            vprintf(b"%d %d %s\n\0".as_ptr() as *const c_char, v);
+        }, 1 as c_int, 2 as c_int, b"salut!\0".as_ptr());
+    }
 }
 
 #[cfg(all(target_os = "windows", target_env = "msvc"))]
